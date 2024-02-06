1171c0a482e56ff757a5d8f8709fd71db4d82a94
Catherine D. Paladines <lcdelgad@bancolombia.com.co>
2024-02-06 18:31:04 -0500

    fix vista_tecnica

--- tasks/checklistPerformance/dist/domain/models/pregunta/pregunta_model.js
+++ tasks/checklistPerformance/dist/domain/models/pregunta/pregunta_model.js
@@ -1,12 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Pregunta = void 0;
-class Pregunta {
-    constructor(id, pregunta, respuestas = []) {
-        this.id = id;
-        this.pregunta = pregunta;
-        this.respuestas = respuestas;
-    }
-}
-exports.Pregunta = Pregunta;
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.Pregunta = void 0;
+    var Pregunta = (function () {
+        function Pregunta(id, pregunta, respuestas) {
+            if (respuestas === void 0) { respuestas = []; }
+            this.id = id;
+            this.pregunta = pregunta;
+            this.respuestas = respuestas;
+        }
+        return Pregunta;
+    }());
+    exports.Pregunta = Pregunta;
+});
 //# sourceMappingURL=pregunta_model.js.map
--- tasks/checklistPerformance/dist/domain/models/respuesta/respuesta_model.js
+++ tasks/checklistPerformance/dist/domain/models/respuesta/respuesta_model.js
@@ -1,12 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Respuesta = void 0;
-class Respuesta {
-    constructor(id, respuesta, preguntasSiguientes = []) {
-        this.id = id;
-        this.respuesta = respuesta;
-        this.preguntasSiguientes = preguntasSiguientes;
-    }
-}
-exports.Respuesta = Respuesta;
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.Respuesta = void 0;
+    var Respuesta = (function () {
+        function Respuesta(id, respuesta, preguntasSiguientes) {
+            if (preguntasSiguientes === void 0) { preguntasSiguientes = []; }
+            this.id = id;
+            this.respuesta = respuesta;
+            this.preguntasSiguientes = preguntasSiguientes;
+        }
+        return Respuesta;
+    }());
+    exports.Respuesta = Respuesta;
+});
 //# sourceMappingURL=respuesta_model.js.map
