0e3a0911635dcb0ac8e61dfe40e09165b37bb009
dacifuen <dacifuen@bancolombia.com.co>
2024-02-08 16:09:02 -0500

    Cambios en el VSS y nueva version para que coja la primera interfaz

---
+++ tasks/checklistPerformance/src/domain/uses_cases/fomulario/formularioPreguntas_useCase.ts
@@ -0,0 +1,16 @@
+/// <reference types="vss-web-extension-sdk" />
+import Extension_Data = require("VSS/SDK/Services/ExtensionData");
+import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
+import Dialogs = require("VSS/Controls/Dialogs")
+
+
+
+
+
+
+
+
+
+
+
+
